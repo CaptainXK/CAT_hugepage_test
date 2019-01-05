ab9f34ade879071f77842619f5d9d73609b52530
xk <permission@hnu.edu.cn>
2019-01-05 21:49:26 +0800

    new time report api

--- time_measurer.h
+++ time_measurer.h
@@ -4,6 +4,15 @@
 #include <stdio.h>
 #include <inttypes.h>
 
+enum{
+	s=0,
+	ms=1,
+	us=2,
+	ns=3
+};
+
+char* unit_str[4] = {"s", "ms", "us", "ns"};
+
 struct timespec begin, end;
 uint64_t dura;
 
@@ -18,9 +27,21 @@ void time_stop(){
 }
 
 static inline
-void time_report(){
+void time_report(int _unit){
     dura = ((int64_t)end.tv_sec*1000000000 + end.tv_nsec) - ((int64_t)begin.tv_sec*1000000000 + begin.tv_nsec);//ns
-    printf("Total time elapsed : %lu ns\n", dura);
+
+	double d_dura = (double)dura;
+
+	switch(_unit){
+		case 0:d_dura /= 1000;//s
+		case 1:d_dura /= 1000;//ms
+		case 2:d_dura /= 1000;//us
+		case 3://ns
+		default:
+			   break;
+	}
+
+    printf("Total time elapsed : %lf %s\n", d_dura, unit_str[_unit]);
 }
 
 #endif
